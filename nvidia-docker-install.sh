4d61128f2d28b94b5e6a534f99b17801de8a7421
Daniel Orf <dorf@suse.com>
2018-08-29 11:30:22 -0700

    Added nvidia-docker-install.sh

---
+++ nvidia-docker-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#Enter transactional-update shell with "transactional-update shell"
+
+mkdir /tmp/nvidia-docker
+cd /tmp/nvidia-docker
+wget https://github.com/danielorf/libnvidia-container/releases/download/v1.0.0-openSUSE423/libnvidia-container-openSUSE423.tar.gz
+wget https://github.com/danielorf/nvidia-container-runtime/releases/download/v1.4.0-openSUSE423/nvidia-container-runtime-openSUSE423.tar.gz
+wget https://github.com/danielorf/nvidia-docker/releases/download/v2.0.3-openSUSE423/nvidia-docker-openSUSE423.tar.gz
+tar -xvf libnvidia-container-openSUSE423.tar.gz
+echo "Installing libnvidia-container-openSUSE423 RPMs"
+zypper in *.rpm
+rm *.rpm
+tar -xvf nvidia-container-runtime-openSUSE423.tar.gz
+echo "Installing nvidia-container-runtime-openSUSE423 RPMs"
+zypper in *.rpm
+rm *.rpm
+tar -xvf nvidia-docker-openSUSE423.tar.gz
+echo "Installing nvidia-docker-openSUSE423 RPMs"
+zypper in *.rpm
+rm *.rpm
+cd ..
+rm -rf /tmp/nvidia-docker
+echo "Finished installing nvidia-docker RPMs"
