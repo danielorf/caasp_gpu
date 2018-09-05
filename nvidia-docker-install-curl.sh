2875c289737000dea2617d332db36215caae46ed
Daniel Orf <dorf@suse.com>
2018-09-04 19:22:15 -0700

    Added podspec with toleration and nvidia docker runtime install script using curl

---
+++ nvidia-docker-install-curl.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This script must be run in transactional-update shell
+
+mkdir /tmp/nvidia-docker
+cd /tmp/nvidia-docker
+curl -k https://github.com/danielorf/libnvidia-container/releases/download/v1.0.0-openSUSE423/libnvidia-container-openSUSE423.tar.gz --output libnvidia-container-openSUSE423.tar.gz
+curl -k https://github.com/danielorf/nvidia-container-runtime/releases/download/v1.4.0-openSUSE423/nvidia-container-runtime-openSUSE423.tar.gz --output nvidia-container-runtime-openSUSE423.tar.gz
+curl -k https://github.com/danielorf/nvidia-docker/releases/download/v2.0.3-openSUSE423/nvidia-docker-openSUSE423.tar.gz --output nvidia-docker-openSUSE423.tar.gz
+tar -xvf libnvidia-container-openSUSE423.tar.gz
+echo "Installing libnvidia-container-openSUSE423 RPMs"
+zypper in *.rpm
+rm *.rpm
+tar -xvf nvidia-container-runtime-openSUSE423.tar.gz
+echo "Installing nvidia-container-runtime-openSUSE423 RPMs"
+zypper in *.rpm
+rm *.rpm
+tar -xvf nvidia-docker-openSUSE423.tar.gz
+echo "Installing nvidia-docker-openSUSE423 RPMs"
+zypper in *.rpm
+rm *.rpm
+cd ..
+rm -rf /tmp/nvidia-docker
+echo "Finished installing nvidia-docker RPMs"
